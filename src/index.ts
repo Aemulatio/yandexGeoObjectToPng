8b26dfe7838a680bc2a2de76f036b96e3c8d3180
Stepan S <dumn2011@gmail.com>
2023-07-19 23:26:52 +0300

    Добавил логику для рисования прямоугольника, написал функцию для подсчет дополнительной прозрачности

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import type {
   GeoObject,
   IPixelPolygonGeometry,
+  IPixelRectangleGeometry,
   IPolygonGeometry,
   Map,
 } from "yandex-maps";
 import { Polygon } from "yandex-maps";
+
+interface Config {
+  fillColor: string;
+  fillOpacity: number;
+  strokeColor: string;
+  strokeOpacity: number;
+  strokeWidth: number;
+  borderRadius: number;
+}
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 class Geo2Png {
@@ -87,38 +97,96 @@ class Geo2Png {
   createPng() {
     if (!this.geoObject || !this.geoObject.geometry) return;
     console.log(this.geoObject.geometry.getType());
-    if (this.zoomToBounds) {
+    if (this.geoObject.geometry.getType() === "Polygon") {
+      if (this.zoomToBounds) {
+        if (!("getPixelGeometry" in this.geoObject.geometry)) return;
+        const bounds = this.geoObject.geometry.getBounds() as number[][];
+
+        const [map, mapContainer] = this._createMap(bounds) as [
+          Map,
+          HTMLDivElement,
+        ];
+        const object = map.geoObjects.getIterator().getNext() as Polygon;
+
+        const pixelBounds = object.geometry
+          ?.getPixelGeometry()
+          .getBounds() as number[][];
+
+        const deltaX = Math.min(pixelBounds[0][0], pixelBounds[1][0]);
+        const deltaY = Math.min(pixelBounds[0][1], pixelBounds[1][1]);
+        const pixelCoordinates = (
+          object.geometry?.getPixelGeometry() as IPixelPolygonGeometry
+        )
+          .getCoordinates()[0]
+          .map((point) => [
+            point[0] - deltaX + this.padding,
+            point[1] - deltaY + this.padding,
+          ]);
+
+        const canvasHeight = Math.round(
+            Math.max(...pixelCoordinates.flatMap((c) => c[1])),
+          ),
+          canvasWidth = Math.round(
+            Math.max(...pixelCoordinates.flatMap((c) => c[0])),
+          );
+        const canvas = this._createCanvas();
+
+        canvas.width = canvasWidth + this.padding;
+        canvas.height = canvasHeight + this.padding;
+        const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+        const config = this.geoObject.options.getAll();
+
+        ctx.strokeStyle =
+          "strokeColor" in config ? (config?.strokeColor as string) : "#0000FF";
+        ctx.lineWidth =
+          "strokeWidth" in config ? (config.strokeWidth as number) : 2;
+        ctx.fillStyle =
+          "fillColor" in config ? (config.fillColor as string) : "#7df9ff33";
+
+        ctx.beginPath();
+        ctx.moveTo(pixelCoordinates[0][0], pixelCoordinates[0][1]);
+
+        for (let i = 1; i < pixelCoordinates.length; i++) {
+          ctx.lineTo(pixelCoordinates[i][0], pixelCoordinates[i][1]);
+        }
+
+        ctx.closePath();
+        ctx.stroke();
+        ctx.fill();
+
+        // console.log(canvas.toDataURL());
+      } else {
+        this._withoutZoom();
+      }
+    } else {
       if (!("getPixelGeometry" in this.geoObject.geometry)) return;
-      const bounds = this.geoObject.geometry.getBounds() as number[][];
 
-      const [map, mapContainer] = this._createMap(bounds) as [
-        Map,
-        HTMLDivElement,
-      ];
-      const object = map.geoObjects.getIterator().getNext() as Polygon;
-
-      const pixelBounds = object.geometry
-        ?.getPixelGeometry()
+      const pixelBounds = this.geoObject.geometry
+        .getPixelGeometry()
         .getBounds() as number[][];
 
       const deltaX = Math.min(pixelBounds[0][0], pixelBounds[1][0]);
       const deltaY = Math.min(pixelBounds[0][1], pixelBounds[1][1]);
+
+      const canvas = this._createCanvas();
+
       const pixelCoordinates = (
-        object.geometry?.getPixelGeometry() as IPixelPolygonGeometry
+        this.geoObject.geometry?.getPixelGeometry() as IPixelRectangleGeometry
       )
-        .getCoordinates()[0]
+        .getCoordinates()
         .map((point) => [
           point[0] - deltaX + this.padding,
           point[1] - deltaY + this.padding,
         ]);
 
+      console.log(pixelCoordinates, deltaX, deltaY);
       const canvasHeight = Math.round(
           Math.max(...pixelCoordinates.flatMap((c) => c[1])),
         ),
         canvasWidth = Math.round(
           Math.max(...pixelCoordinates.flatMap((c) => c[0])),
         );
-      const canvas = this._createCanvas();
 
       canvas.width = canvasWidth + this.padding;
       canvas.height = canvasHeight + this.padding;
@@ -126,27 +194,29 @@ class Geo2Png {
 
       const config = this.geoObject.options.getAll();
 
-      ctx.strokeStyle =
-        "strokeColor" in config ? (config?.strokeColor as string) : "#0000FF";
+      console.log(config);
+      const strokeStyle =
+        this._calculateStyle(config, "strokeColor", "strokeOpacity") ||
+        "rgba(0,0,255,0.1)";
+
+      console.log("strokeStyle: ", strokeStyle);
+      ctx.strokeStyle = strokeStyle;
       ctx.lineWidth =
         "strokeWidth" in config ? (config.strokeWidth as number) : 2;
       ctx.fillStyle =
-        "fillColor" in config ? (config.fillColor as string) : "#7df9ff33";
+        this._calculateStyle(config, "fillColor", "fillOpacity") || "#7df9ff33";
 
       ctx.beginPath();
-      ctx.moveTo(pixelCoordinates[0][0], pixelCoordinates[0][1]);
+      ctx.roundRect(
+        pixelCoordinates[0][0],
+        pixelCoordinates[0][1],
+        pixelCoordinates[1][0] - pixelCoordinates[0][0],
+        pixelCoordinates[1][1] - pixelCoordinates[0][1],
+        "borderRadius" in config ? (config.borderRadius as number) : 0,
+      );
 
-      for (let i = 1; i < pixelCoordinates.length; i++) {
-        ctx.lineTo(pixelCoordinates[i][0], pixelCoordinates[i][1]);
-      }
-
-      ctx.closePath();
       ctx.stroke();
       ctx.fill();
-
-      // console.log(canvas.toDataURL());
-    } else {
-      this._withoutZoom();
     }
   }
 
@@ -207,4 +277,33 @@ class Geo2Png {
 
     // document.body.removeChild(canvas);
   }
+
+  _calculateStyle = (
+    config: Config,
+    colorProperty: "fillColor" | "strokeColor",
+    opacityProperty: "fillOpacity" | "strokeOpacity",
+  ) => {
+    let returnValue = "";
+    if (colorProperty in config) {
+      console.log("stroke");
+      returnValue = config[colorProperty];
+      if ("strokeOpacity" in config) {
+        if (returnValue.length === 9) {
+          const hex = config[colorProperty];
+          const opacity =
+            (parseInt(returnValue.slice(7), 16) / 255) *
+            config[opacityProperty];
+
+          const r = parseInt(hex.slice(1, 3), 16),
+            g = parseInt(hex.slice(3, 5), 16),
+            b = parseInt(hex.slice(5, 7), 16);
+          return `rgba(${r},${g},${b},${opacity})`;
+        } else {
+          return config[colorProperty];
+        }
+      }
+      return returnValue;
+    }
+    return returnValue;
+  };
 }
